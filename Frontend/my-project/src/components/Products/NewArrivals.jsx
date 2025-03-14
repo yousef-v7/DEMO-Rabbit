472a47e52ea0127d8268b979205287dfcec71989
yousef <yousef.ahmed7@gmail.com>
2025-03-14 09:41:23 +0200

    Scrollable padding

--- Frontend/my-project/src/components/Products/NewArrivals.jsx
+++ Frontend/my-project/src/components/Products/NewArrivals.jsx
@@ -165,13 +165,13 @@ const NewArrivals = () => {
     <section className="py-16 px-4 lg:px-0">
       <div className="container mx-auto text-center mb-10 relative">
         <h2 className="text-3xl font-bold mb-4">Explore New Arrivals</h2>
-        <p className="text-lg text-gray-600 mb-8">
+        <p className="text-lg text-gray-600 mb-8 ">
           Discover the latest styles straight off the runway, freshly added to
           keep your wardrobe on the cutting edge of fashion.
         </p>
 
         {/* Scroll Buttons */}
-        <div className="absolute right-8 bottom-[-30px] flex space-x-2">
+        <div className="absolute right-8 bottom-[-70px] flex space-x-2">
           <button
             onClick={() => scroll("left")}
             disabled={!canScrollLeft}
@@ -200,7 +200,7 @@ const NewArrivals = () => {
       {/* Scrollable Contents */}
       <div
         ref={scrollRef}
-        className={`container mx-auto overflow-x-scroll hide-scrollbar flex space-x-6 relative ${
+        className={`container mx-auto overflow-x-scroll hide-scrollbar flex space-x-6 relative pt-14 ${
           isDragging ? "cursor-grabbing" : "cursor-grab"
         }`}
         style={{ scrollbarWidth: "none", msOverflowStyle: "none" }}
